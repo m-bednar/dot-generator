3786df16742eb387b687632b7321ad49c6a85888
m-bednar <mbedna8@seznam.cz>
2024-07-26 18:06:14 +0200

    use stringifycation for options

--- src/options.ts
+++ src/options.ts
@@ -5,13 +5,20 @@ export type Color = string;
 export type BgColor = string;
 export type Style = string;
 
+function stringifyValue(value: any): string {
+    if (typeof value === 'string') {
+        return `"${value}"`;
+    }
+    return String(value);
+}
+
 export function generateStandaloneOptions(options: object, indent: number) {
     const entries = Object.entries(options).map(([name, value]) => `${makeIndent(indent)}${name}="${value}"`);
     return `${entries.join(';\n')};\n`;
 }
 
 export function generateArgumentOptions(options: object) {
-    const entries = Object.entries(options).map(([name, value]) => `${name}="${value}"`);
+    const entries = Object.entries(options).map(([name, value]) => `${name}=${stringifyValue(value)}`);
     return `[${entries.join(', ')}]`;
 }
 
